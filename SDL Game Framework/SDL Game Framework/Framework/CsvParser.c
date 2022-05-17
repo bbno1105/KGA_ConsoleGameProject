cea72706ca7f1a4c94c789ef7dc4e0e86429dafc
M0000000N <103083005+M0000000N@users.noreply.github.com>
2022-05-17 23:47:53 +0900

    Feat. 수연언니꺼 복사해서 커밋

--- SDL Game Framework/SDL Game Framework/Framework/CsvParser.c
+++ SDL Game Framework/SDL Game Framework/Framework/CsvParser.c
@@ -21,253 +21,254 @@ wchar_t save[500] = L"";
 
 wchar_t* ConvertCtoWC(char* str)
 {
-    static wchar_t unicode[500] = L"";
-    int strLen = MultiByteToWideChar(CP_ACP, 0, str, -1, NULL, NULL);
-    MultiByteToWideChar(CP_ACP, 0, str, strlen(str), unicode, strLen);
-    return unicode;
+	static wchar_t unicode[500] = L"";
+	int strLen = MultiByteToWideChar(CP_ACP, 0, str, -1, NULL, NULL);
+	MultiByteToWideChar(CP_ACP, 0, str, strlen(str), unicode, strLen);
+	return unicode;
 }
 
 wchar_t* ParseCsv(wchar_t* csvStr, wchar_t* saveStr)
 {
-    bool isString = false;
-    while (true)
-    {
-        if (isString)
-        {
-            if (*csvStr == L'"' && *(csvStr + 1) == L'"')
-            {
-                *saveStr = *csvStr;
-                saveStr++; csvStr++; csvStr++;
-            }
-            else if (*csvStr == L'"')
-            {
-                isString = false;
-                csvStr++;
-            }
-            else if (*csvStr == L'\n')
-            {
-                *saveStr = *csvStr;
-                saveStr++; csvStr++;
-                csv = fgets(csv, 500, fp);
-                csvStr = ConvertCtoWC(csv);
-                continue;
-            }
-            else
-            {
-                *saveStr = *csvStr;
-                saveStr++; csvStr++;
-            }
-        }
-        else
-        {
-            if (*csvStr == L'"')
-            {
-                isString = true;
-                csvStr++;
-            }
-            else if (*csvStr == L',' || *csvStr == L'\n')
-            {
-                *saveStr = NULL;
-                *saveStr++; *csvStr++;
-                break;
-            }
-            else
-            {
-                *saveStr = *csvStr;
+	bool isString = false;
+	while (true)
+	{
+		if (isString)
+		{
+			if (*csvStr == L'"' && *(csvStr + 1) == L'"')
+			{
+				*saveStr = *csvStr;
+				saveStr++; csvStr++; csvStr++;
+			}
+			else if (*csvStr == L'"')
+			{
+				isString = false;
+				csvStr++;
+			}
+			else if (*csvStr == L'\n')
+			{
+				*saveStr = *csvStr;
+				saveStr++; csvStr++;
+				csv = fgets(csv, 500, fp);
+				csvStr = ConvertCtoWC(csv);
+				continue;
+			}
+			else
+			{
+				*saveStr = *csvStr;
+				saveStr++; csvStr++;
+			}
+		}
+		else
+		{
+			if (*csvStr == L'"')
+			{
+				isString = true;
+				csvStr++;
+			}
+			else if (*csvStr == L',' || *csvStr == L'\n')
+			{
+				*saveStr = NULL;
+				*saveStr++; *csvStr++;
+				break;
+			}
+			else
+			{
+				*saveStr = *csvStr;
 
-                saveStr++; csvStr++;
-            }
-        }
-    }
-    return csvStr;
+				saveStr++; csvStr++;
+			}
+		}
+	}
+	return csvStr;
 }
 
 void CsvParse_Init(void)
 {
-    fp = fopen("Data.csv", "rt");
+	fp = fopen("Data.csv", "rt");
 
-    setlocale(LC_ALL, "kr_KR.utf8");
+	setlocale(LC_ALL, "kr_KR.utf8");
 
-    csv = malloc(sizeof(int) * 500);
-    wchar_csv = malloc(sizeof(int) * 500);
+	csv = malloc(sizeof(int) * 500);
+	wchar_csv = malloc(sizeof(int) * 500);
 
-    int idNum = 0;
-    int dataNum = 0;
+	int idNum = 0;
+	int dataNum = 0;
 
-    while (true)
-    {
-        csv = fgets(csv, 500, fp);
-        if (feof(fp)) break;
+	while (true)
+	{
+		csv = fgets(csv, 500, fp);
+		if (feof(fp)) break;
 
-        wchar_csv = ConvertCtoWC(csv);
+		wchar_csv = ConvertCtoWC(csv);
 
-        while (*wchar_csv != NULL)
-        {
-            wchar_csv = ParseCsv(wchar_csv, save);
+		while (*wchar_csv != NULL)
+		{
+			wchar_csv = ParseCsv(wchar_csv, save);
 
-            if (idNum > 0)
-            {
-                // @@@@@@@@@ 형식에 맞게 여기를 수정해야함 @@@@@@@@@
-                switch (dataNum)
-                {
-                case 0:
-                    Data[idNum].ID = atoi(save);
-                    break;
+			if (idNum > 0)
+			{
+				// @@@@@@@@@ 형식에 맞게 여기를 수정해야함 @@@@@@@@@
+				switch (dataNum)
+				{
+					case 0:
+						Data[idNum].ID = atoi(save);
+						break;
 
-                case 1:
-                {
-                    int stringNum = 0;   int i = 0;   int j = 0;
-                    while (true)
-                    {
-                        if (save[i] == L'\n')
-                        {
-                            stringNum++; i++; j = 0;
-                            continue;
-                        }
-                        if (save[i] == NULL) break;
-                        Data[idNum].ScenePage[stringNum][j] = save[i];
-                        i++; j++;
-                    }
-                }
-                break;
+					case 1:
+					{
+						int stringNum = 0;	int i = 0;	int j = 0;
+						while (true)
+						{
+							if (save[i] == L'\n')
+							{
+								stringNum++; i++; j = 0;
+								continue;
+							}
+							if (save[i] == NULL) break;
+							Data[idNum].ScenePage[stringNum][j] = save[i];
+							i++; j++;
+						}
+					}
+					break;
 
-                case 2:
-                {
-                    int stringNum = 0;   int i = 0;   int j = 0;
-                    while (true)
-                    {
-                        if (save[i] == L'\n')
-                        {
-                            stringNum++; i++; j = 0;
-                            continue;
-                        }
-                        if (save[i] == NULL) break;
-                        Data[idNum].Text[stringNum][j] = save[i];
-                        i++; j++;
-                    }
-                }
-                break;
+					case 2:
+					{
+						int stringNum = 0;	int i = 0;	int j = 0;
+						while (true)
+						{
+							if (save[i] == L'\n')
+							{
+								stringNum++; i++; j = 0;
+								continue;
+							}
+							if (save[i] == NULL) break;
+							Data[idNum].Text[stringNum][j] = save[i];
+							i++; j++;
+						}
+					}
+					break;
 
-                case 3:
-                    Data[idNum].Select1 = atoi(save);
-                    break;
+					case 3:
+						Data[idNum].Select1 = atoi(save);
+						break;
 
-                case 4:
-                {
-                    int stringNum = 0;   int i = 0;   int j = 0;
-                    while (true)
-                    {
-                        if (save[i] == L'\n')
-                        {
-                            stringNum++; i++; j = 0;
-                            continue;
-                        }
-                        if (save[i] == NULL) break;
-                        Data[idNum].MovingPage1[stringNum][j] = save[i];
-                        i++; j++;
-                    }
-                }
-                break;
+					case 4:
+					{
+						int stringNum = 0;	int i = 0;	int j = 0;
+						while (true)
+						{
+							if (save[i] == L'\n')
+							{
+								stringNum++; i++; j = 0;
+								continue;
+							}
+							if (save[i] == NULL) break;
+							Data[idNum].MovingPage1[stringNum][j] = save[i];
+							i++; j++;
+						}
+					}
+					break;
 
-                case 5:
-                    Data[idNum].Select2 = atoi(save);
-                    break;
+					case 5:
+						Data[idNum].Select2 = atoi(save);
+						break;
 
-                case 6:
-                {
-                    int stringNum = 0;   int i = 0;   int j = 0;
-                    while (true)
-                    {
-                        if (save[i] == L'\n')
-                        {
-                            stringNum++; i++; j = 0;
-                            continue;
-                        }
-                        if (save[i] == NULL) break;
-                        Data[idNum].MovingPage2[stringNum][j] = save[i];
-                        i++; j++;
-                    }
-                }
-                break;
+					case 6:
+					{
+						int stringNum = 0;	int i = 0;	int j = 0;
+						while (true)
+						{
+							if (save[i] == L'\n')
+							{
+								stringNum++; i++; j = 0;
+								continue;
+							}
+							if (save[i] == NULL) break;
+							Data[idNum].MovingPage2[stringNum][j] = save[i];
+							i++; j++;
+						}
+					}
+					break;
 
-                case 7:
-                    Data[idNum].Select3 = atoi(save);
-                    break;
+					case 7:
+						Data[idNum].Select3 = atoi(save);
+						break;
 
-                case 8:
-                {
-                    int stringNum = 0;   int i = 0;   int j = 0;
-                    while (true)
-                    {
-                        if (save[i] == L'\n')
-                        {
-                            stringNum++; i++; j = 0;
-                            continue;
-                        }
-                        if (save[i] == NULL) break;
-                        Data[idNum].MovingPage3[stringNum][j] = save[i];
-                        i++; j++;
-                    }
-                }
-                break;
+					case 8:
+					{
+						int stringNum = 0;	int i = 0;	int j = 0;
+						while (true)
+						{
+							if (save[i] == L'\n')
+							{
+								stringNum++; i++; j = 0;
+								continue;
+							}
+							if (save[i] == NULL) break;
+							Data[idNum].MovingPage3[stringNum][j] = save[i];
+							i++; j++;
+						}
+					}
+					break;
 
-                case 9:
-                {
-                    int stringNum = 0;   int i = 0;   int j = 0;
-                    while (true)
-                    {
-                        if (save[i] == L'\n')
-                        {
-                            stringNum++; i++; j = 0;
-                            continue;
-                        }
-                        if (save[i] == NULL) break;
-                        Data[idNum].SoundFile[stringNum][j] = save[i];
-                        i++; j++;
-                    }
-                }
-                break;
+					case 9:
+					{
+						int stringNum = 0;	int i = 0;	int j = 0;
+						while (true)
+						{
+							if (save[i] == L'\n')
+							{
+								stringNum++; i++; j = 0;
+								continue;
+							}
+							if (save[i] == NULL) break;
+							Data[idNum].SoundFile[stringNum][j] = save[i];
+							i++; j++;
+						}
+					}
+					break;
 
-                case 10:
-                {
+					case 10:
+					{
 
-                    int stringNum = 0;   int i = 0;   int j = 0;
-                    while (true)
-                    {
-                        if (save[i] == L'\n')
-                        {
-                            stringNum++; i++; j = 0;
-                            continue;
-                        }
-                        if (save[i] == NULL) break;
-                        Data[idNum].ImageFile[stringNum][j] = save[i];
-                        i++; j++;
-                    }
-                }
-                break;
-                }
-            }
-            dataNum++;
-        }
-        dataNum = 0;
-        idNum++;
-    }
+						int stringNum = 0;	int i = 0;	int j = 0;
+						while (true)
+						{
+							if (save[i] == L'\n')
+							{
+								stringNum++; i++; j = 0;
+								continue;
+							}
+							if (save[i] == NULL) break;
+							Data[idNum].ImageFile[stringNum][j] = save[i];
+							i++; j++;
+						}
+					}
+					break;
+				}
+			}
+			dataNum++;
+		}
+		dataNum = 0;
+		idNum++;
+	}
 
-    printf("%d\n", sizeof(Data));
+	printf("%d\n", sizeof(Data));
 
-    fclose(fp);
-    free(csv);
+	fclose(fp);
+	free(csv);
 }
 
 // 사용법 : Data[GetData(10)].String << ID가 10인 값의 String을 불러온다.
 int GetCsvData(int ID)
 {
-    for (int i = 0; i < 100; i++)
-    {
-        if (Data[i].ID == ID)
-        {
-            return i;
-        }
-    }
-    return 0;
+	for (int i = 0; i < 100; i++)
+	{
+		if (Data[i].ID == ID)
+		{
+			return i;
+		}
+	}
+	return 0;
 }
+
